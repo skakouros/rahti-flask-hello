53e1a887c7ad3ad61d2092e1b20fad137cd8856a
skakouros <46928318+skakouros@users.noreply.github.com>
2024-05-16 11:18:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ import flask
 # In a proper Flask application all these templates should be in indepent files
 STYLE = """
 body {
-  background-color: silver;
+  background-color: beige;
   font-family: "Helvetica Neue",Helvetica,"Liberation Sans",Arial,sans-serif;
-  font-size: 14px;
+  font-size: 16px;
   padding: 10%;
 }
 img {
